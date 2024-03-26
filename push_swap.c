bcd51d597de96cef02d9596752eda81cfb461a53
rachneum <rachel-1604@hotmail.com>
2024-03-26 14:19:04 +0100

    test18

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: rachou <rachou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 15:04:21 by rachou            #+#    #+#             */
-/*   Updated: 2024/03/26 13:06:25 by rachou           ###   ########.fr       */
+/*   Updated: 2024/03/26 14:17:18 by rachou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,25 @@
 int	target_node_b(t_stack *a, t_stack *b)//Fonction qui me permet de trouver les noeuds target dans ma stack 'b' pour les noeuds de la stack 'a'.
 {
     t_stack *current_b;//Permet de me servir de ma stack 'b' sans la modifier (c'est une sorte de copie)
+    t_stack *current_target_node;
     long    target;
 
     while (a)
     {
         current_b = b;
-        target = LONG_MAX;
+        current_target_node = target_node;
+        target = LONG_MIN;
 	    while (current_b)
         {
-            if (a->value > current_b->value)
-                a->target_node = current_b->value;
+            if ((a->value > current_b->value) && (target < current_b->value))
+            {
+                a->current_target_node = current_b->value;
                 target = current_b->value;
-            if ((a->value > current_b->value) && (a->target_node < current_b->value))
-                a->target_node = current_b->value;
+            }
             current_b = current_b->next;
         }
-        if (target == LONG_MAX)
-            a->target_node = find_max(b);//Dans le cas où mon noeud 'a' ne trouve pas un noeud target le plus proche petit, j'irai trouver le plus proche grand à la place.
+        if (target == LONG_MIN)
+            a->current_target_node = find_max(b);//Dans le cas où mon noeud 'a' ne trouve pas un noeud target le plus proche petit, j'irai trouver le plus proche grand à la place.
         else
 
 
@@ -42,25 +44,26 @@ int	target_node_b(t_stack *a, t_stack *b)//Fonction qui me permet de trouver les
 int	target_node_a(t_stack *a, t_stack *b)//Fonction qui me permet de trouver les noeuds target dans ma stack 'a' pour les noeuds de la stack 'b'.
 {
     t_stack *current_a;//Permet de me servir de ma stack 'a' sans la modifier (c'est une sorte de copie)
+    t_stack *current_target_node;
     long    target;
 
     while (b)
     {
         current_a = a;
+        current_target_node = target_node;
         target = LONG_MAX;
 	    while (current_a)
         {
-            if (b->value > current_a->value)
-                b->target_node = current_a->value;
+            if ((b->value < current_a->value) && (target > current_a->value))
+            {
+                b->current_target_node = current_a->value;
                 target = current_a->value;
-            if ((b->value > current_a->value) && (b->target_node < current_a->value))
-                b->target_node = current_a->value;
+            }
             current_a = current_a->next;
         }
         if (target == LONG_MAX)
-            b->target_node = find_max(a);//Dans le cas où mon noeud 'b' ne trouve pas un noeud target le plus proche petit, j'irai trouver le plus proche grand à la place.
+            b->current_target_node = find_max(a);//Dans le cas où mon noeud 'b' ne trouve pas un noeud target le plus proche petit, j'irai trouver le plus proche grand à la place.
         else
-
 
         b = b->next;
     }
