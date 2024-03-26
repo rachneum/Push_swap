b465eeb6d59e31c9fe4a41daa7dc389fdacd4154
rachneum <rachel-1604@hotmail.com>
2024-03-26 15:20:28 +0100

    test19

--- push_swap.c
+++ push_swap.c
@@ -6,11 +6,11 @@
 /*   By: rachou <rachou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 15:04:21 by rachou            #+#    #+#             */
-/*   Updated: 2024/03/26 14:17:18 by rachou           ###   ########.fr       */
+/*   Updated: 2024/03/26 15:19:49 by rachou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+#include "push_swap.h"
 
 int	target_node_b(t_stack *a, t_stack *b)//Fonction qui me permet de trouver les noeuds target dans ma stack 'b' pour les noeuds de la stack 'a'.
 {
@@ -62,7 +62,7 @@ int	target_node_a(t_stack *a, t_stack *b)//Fonction qui me permet de trouver les
             current_a = current_a->next;
         }
         if (target == LONG_MAX)
-            b->current_target_node = find_max(a);//Dans le cas où mon noeud 'b' ne trouve pas un noeud target le plus proche petit, j'irai trouver le plus proche grand à la place.
+            b->current_target_node = find_min(a);//Dans le cas où mon noeud 'b' ne trouve pas un noeud target le plus proche grand, j'irai trouver le plus proche petit à la place.
         else
 
         b = b->next;
--- push_swap_utils.c
+++ push_swap_utils.c
@@ -6,7 +6,7 @@
 /*   By: rachou <rachou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 12:11:05 by rachou            #+#    #+#             */
-/*   Updated: 2024/03/26 09:57:49 by rachou           ###   ########.fr       */
+/*   Updated: 2024/03/26 15:18:11 by rachou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ int	stack_len(t_stack_node *stack)
 	}
 	return (count);
 }
-/*
+
 t_stack	*find_min(t_stack *stack)//Recherche une pile et renvoie le noeud avec le plus petit nombre.
 {
 	long	min;//Pour stocker la plus petite valeur jusqu'à présent.
@@ -42,35 +42,35 @@ t_stack	*find_min(t_stack *stack)//Recherche une pile et renvoie le noeud avec l
 
 	if (!stack)
 		return (NULL);
-	min = LONG_MAX;//Attribue à la plus petite valeur jusqu'à présent, l'entier long maximum
+	min = LONG_MAX;//Attribue à la plus petite valeur jusqu'à présent, l'entier long maximum.
 	while (stack)
 	{
-		if (stack->nbr < min) //Check if the current node value is smaller than the smallest so far
+		if (stack->nbr < min)//Vérifie si la valeur actuelle du noeud est inférieure à la plus petite jusqu'à présent.
 		{
-			min = stack->nbr; //If so, update the smallest number so far
-			min_node = stack; //Set the pointer to point to the node with the smallest number so far
+			min = stack->nbr;//Si c'est le cas, mettre à jour le plus petit nombre jusqu'à présent.
+			min_node = stack;//Définit le pointeur pour qu'il pointe vers le noeud avec le plus petit nombre jusqu'à présent.
 		}
-		stack = stack->next; //Move to the next node for processing
+		stack = stack->next;
 	}
 	return (min_node); 
 }
 
-t_stack	*find_max(t_stack *stack) //Define a function that searches a stack and returns the node with the biggest number
+t_stack	*find_max(t_stack *stack)//Recherche une pile et renvoie le noeud avec le plus grand nombre.
 {
-	long			max; //To store the biggest value so far
-	t_stack	*max_node; //To store a pointer that points to the biggest number
+	long	max;//Pour stocker la plus grande valeur jusqu'à présent.
+	t_stack	*max_node;//Pour stocker un pointeur qui pointe vers le plus grand nombre.
 
 	if (!stack)
 		return (NULL);
-	max = LONG_MIN; //Assign to the biggest value so far, the max long integer
-	while (stack) //Loop until the end of the stack is reached
+	max = LONG_MIN;//Attribue à la plus grande valeur jusqu'à présent, l'entier long maximum.
+	while (stack)
 	{
-		if (stack->nbr > max) //Check if the current node value is smaller than the biggest so far
+		if (stack->nbr > max)//Vérifie si la valeur actuelle du noeud est inférieure à la plus grande jusqu'à présent.
 		{
-			max = stack->nbr; //If so, update the biggest number so far
-			max_node = stack; //Set the pointer to point to the node with the biggest number so far
+			max = stack->nbr;//Si c'est le cas, mettre à jour le plus grand nombre jusqu'à présent.
+			max_node = stack;//Définit le pointeur pour qu'il pointe vers le noeud avec le plus grand nombre jusqu'à présent.
 		}
-		stack = stack->next; //Move to the next node for processing
+		stack = stack->next;
 	}
 	return (max_node);
-}*/
+}
